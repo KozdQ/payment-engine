1db7e7b8b84d1c22fe78c7dd4ac2429951491a26
LAP15863 <dang.hbaokody@gmail.com>
2022-09-14 18:40:13 +0700

    init

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $org.kord;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
